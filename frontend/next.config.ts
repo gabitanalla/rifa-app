a0a91b2747a08c493f520eee227919edfa3c73c3
gabitanalla <g.golmar@gmail.com>
2025-11-30 16:30:28 -0300

    Fix netx config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/rifa-app',
+  assetPrefix: '/rifa-app/',
 };
 
 export default nextConfig;
