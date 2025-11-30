2f07cbb136ee6ed520748b5738655f920f761ea9
gabitanalla <g.golmar@gmail.com>
2025-11-30 16:37:00 -0300

    Fix fetch parameter

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        participant: participants,
+        participants: participants,
         prices: prices,
       }),
     }).then(response => response.json())
